7e8ecb75796658e85d5d6f6d6919e0cd4cb3cf1c
Shrikant Gutte <shrikantgutte@hotmail.com>
2023-03-23 18:34:04 +0000

    Minor fixes in db scripts

--- backend-flask/db/schema.sql
+++ backend-flask/db/schema.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS public.activities;
 
 CREATE TABLE public.users (
   uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-  display_name UUID NOT NULL,
+  display_name text NOT NULL,
   handle text,
   cognito_user_id text,
   created_at TIMESTAMP default current_timestamp NOT NULL
@@ -13,6 +13,7 @@ CREATE TABLE public.users (
 
 CREATE TABLE public.activities (
   uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  user_uuid UUID NOT NULL,
   message text NOT NULL,
   replies_count integer DEFAULT 0,
   reposts_count integer DEFAULT 0,
