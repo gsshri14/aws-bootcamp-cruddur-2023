21c88a01cff402e3d7df8b05c00ca3b6b631664e
Shrikant Gutte <shrikantgutte@hotmail.com>
2023-04-02 23:53:35 +0000

    minor fixes

--- backend-flask/services/create_activity.py
+++ backend-flask/services/create_activity.py
@@ -1,11 +1,9 @@
 from datetime import datetime, timedelta, timezone
+
 from lib.db import db
 
 class CreateActivity:
-
-  @staticmethod
   def run(message, user_handle, ttl):
-
     model = {
       'errors': None,
       'data': None
@@ -45,25 +43,22 @@ class CreateActivity:
       }   
     else:
       expires_at = (now + ttl_offset)
-      uuid = CreateActivity.create_activity(user_handle, message, expires_at)
+      uuid = CreateActivity.create_activity(user_handle,message,expires_at)
+
       object_json = CreateActivity.query_object_activity(uuid)
       model['data'] = object_json
     return model
 
-
-  @staticmethod
   def create_activity(handle, message, expires_at):
-    sql = db.template('activities', 'create')
-    uuid = db.query_commit(sql, {
+    sql = db.template('activities','create')
+    uuid = db.query_commit(sql,{
       'handle': handle,
       'message': message,
       'expires_at': expires_at
     })
     return uuid
-
-  @staticmethod
   def query_object_activity(uuid):
-    sql = db.template('activities', 'object')
-    return db.query_object_json(sql, {
+    sql = db.template('activities','object')
+    return db.query_object_json(sql,{
       'uuid': uuid
     })
